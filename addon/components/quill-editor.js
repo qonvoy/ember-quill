c64d47c2356ebf17b6db82858ede5843d566f365
Jeffrey Gelens <jeffrey@noppo.pro>
2016-10-31 17:33:17 +0100

    added Ember actions

--- addon/components/quill-editor.js
+++ addon/components/quill-editor.js
@@ -3,32 +3,27 @@
 import Ember from "ember";
 import layout from "../templates/components/quill-editor";
 
-import { run } from Ember;
+const { computed, run, get, set } = Ember;
 
 export default Ember.Component.extend({
   layout,
+  editor: null,
+
+  options: computed(function() {
+    return {theme: "snow"}
+  }),
 
   didInsertElement() {
-    const options = {
-      theme: "snow",
-      modules: {
-        toolbar: [
-          [{header: [2, 3, 4, false]}],
-          ["bold", "italic", "underline", "strike"],
-          [{"color": []}],
-          [{"list": "ordered"}, {"list": "bullet"}],
-          [{"indent": "-1"}, {"indent": "+1"}],
-          [{"align": []}],
-          ["link"],
-          ["clean"]
-        ]
-      }
-    }
-    const editor = new Quill(this.$().get(0), options);
-    editor.on("text-change", function() {
-      run.debounce(this, () => {
-        this.attrs.update(quill.root.innerHtml);
-      }, 250);
-    })
+    const self = this;
+    const editor = new Quill(this.$().get(0), get(this, "options"));
+
+    editor.on("text-change", (delta, oldDelta, source) => {
+      self.sendAction("textChange", get(self, "editor"), delta, oldDelta, source);
+    });
+    editor.on("selection-change", (delta, oldDelta, source) => {
+      self.sendAction("selectionChange", get(self, "editor"), delta, oldDelta, source);
+    });
+
+    set(this, "editor", editor);
   }
 });
