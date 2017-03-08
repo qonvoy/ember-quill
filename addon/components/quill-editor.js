32897ea5ce05c929f6f098df9e2219d2d5c33841
Jeffrey Gelens <jeffrey@noppo.pro>
2017-03-08 15:56:48 +0100

    v0.0.6

--- addon/components/quill-editor.js
+++ addon/components/quill-editor.js
@@ -17,8 +17,14 @@ export default Component.extend({
     return Ember.String.htmlSafe(get(this, "value"));
   }),
 
+  // didUpdateAttrs() {
+  //   this._super(...arguments);
+  //   // get(this, "editor").clipboard.dangerouslyPasteHTML()
+  // }
+
   didInsertElement() {
     const self = this;
+    const html = this.$().get(0);
     const editor = new Quill(this.$().get(0), get(this, "options"));
 
     editor.on("text-change", (delta, oldDelta, source) => {
