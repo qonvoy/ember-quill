b69f00087a88bce7baf86c55a35a99d77f0ac5d4
Christian Spanring <cspanring@gmail.com>
2017-04-27 14:21:46 -0400

    adds fastboot compatibility

--- addon/components/quill-editor.js
+++ addon/components/quill-editor.js
@@ -1,20 +1,25 @@
 /* global Quill */
 
-import Ember from "ember";
-import layout from "../templates/components/quill-editor";
+import Ember from 'ember';
+import layout from '../templates/components/quill-editor';
 
-const { Component, computed, get, set } = Ember;
+const {Component, computed, get, set, getOwner} = Ember;
 
 export default Component.extend({
   layout,
   editor: null,
 
   options: computed(function() {
-    return {theme: "snow"};
+    return {theme: 'snow'};
   }),
 
   safeValue: computed(function() {
-    return Ember.String.htmlSafe(get(this, "value"));
+    return Ember.String.htmlSafe(get(this, 'value'));
+  }),
+
+  fastboot: computed(function() {
+    let owner = getOwner(this);
+    return owner.lookup('service:fastboot');
   }),
 
   // didUpdateAttrs() {
@@ -23,17 +28,34 @@ export default Component.extend({
   // }
 
   didInsertElement() {
-    const self = this;
-    const html = this.$().get(0);
-    const editor = new Quill(this.$().get(0), get(this, "options"));
+    // don't instantiate Quill if fastboot is detected
+    const isFastBoot = this.get('fastboot.isFastBoot');
 
-    editor.on("text-change", (delta, oldDelta, source) => {
-      self.sendAction("textChange", get(self, "editor"), delta, oldDelta, source);
-    });
-    editor.on("selection-change", (delta, oldDelta, source) => {
-      self.sendAction("selectionChange", get(self, "editor"), delta, oldDelta, source);
-    });
+    if (!isFastBoot) {
+      const self = this;
+      const html = this.$().get(0);
+      const editor = new Quill(this.$().get(0), get(this, 'options'));
 
-    set(this, "editor", editor);
-  }
+      editor.on('text-change', (delta, oldDelta, source) => {
+        self.sendAction(
+          'textChange',
+          get(self, 'editor'),
+          delta,
+          oldDelta,
+          source
+        );
+      });
+      editor.on('selection-change', (delta, oldDelta, source) => {
+        self.sendAction(
+          'selectionChange',
+          get(self, 'editor'),
+          delta,
+          oldDelta,
+          source
+        );
+      });
+
+      set(this, 'editor', editor);
+    }
+  },
 });
