b9b0808b15555dbb16b0ec92fe6af32f298c059c
Jeffrey Gelens <jeffrey@noppo.pro>
2018-09-27 00:12:07 +0200

    Do testing with Firefox

--- testem.js
+++ testem.js
@@ -1,24 +1,17 @@
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
+  test_page: "tests/index.html?hidepassed",
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    "Firefox"
   ],
   launch_in_dev: [
-    'Chrome'
+    "Firefox"
   ],
   browser_args: {
-    Chrome: {
+    Firefox: {
       ci: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.CI ? '--no-sandbox' : null,
-        '--headless',
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-software-rasterizer',
-        '--mute-audio',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900'
+        "--headless",
+        "--window-size=1440,900"
       ].filter(Boolean)
     }
   }
