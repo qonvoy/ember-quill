d4b0a7b5801c9e5d7ddcb7735902fab00742062d
Jeffrey Gelens <jeffrey@noppo.pro>
2019-03-20 17:05:07 +0100

    Start using ember-auto-import

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ module.exports = {
   },
   included(app) {
     this._super.included(app);
-    app.import("node_modules/quill/dist/quill.js");
     app.import("node_modules/quill/dist/quill.snow.css");
-    app.import("vendor/quill.js", {
-      exports: {
-        Quill: ["default"]
-      }
-    });
-  },
+  }
 };
